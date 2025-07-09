526a8c342d616ef9d81c5fb4ca21cd2eb738cad7
tacowasa059 <wasserstein59@gmail.com>
2025-07-09 23:44:12 +0900

    refactor: プロジェクト構造の変更

---
+++ common/src/main/java/jp/axer/cocoainput/CocoaInput.java
@@ -0,0 +1,98 @@
+package jp.axer.cocoainput;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.apache.commons.io.IOUtils;
+
+import com.sun.jna.Platform;
+
+import jp.axer.cocoainput.arch.darwin.DarwinController;
+import jp.axer.cocoainput.arch.dummy.DummyController;
+import jp.axer.cocoainput.arch.win.WinController;
+import jp.axer.cocoainput.arch.x11.X11Controller;
+import jp.axer.cocoainput.plugin.CocoaInputController;
+import jp.axer.cocoainput.util.ConfigPack;
+import jp.axer.cocoainput.util.ModLogger;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+
+public class CocoaInput {
+	private static CocoaInputController controller;
+	private static String zipsource;
+	public static ConfigPack config = ConfigPack.defaultConfig;
+	
+	public CocoaInput(String loader, String zipfile) {
+		ModLogger.log("Modloader:" + loader);
+		CocoaInput.zipsource = zipfile;
+		try {
+			if (Platform.isMac()) {
+				CocoaInput.applyController(new DarwinController());
+			} else if (Platform.isWindows()) {
+				CocoaInput.applyController(new WinController());
+			} else if (Platform.isX11()) {
+				CocoaInput.applyController(new X11Controller());
+			} else {
+				ModLogger.log("CocoaInput cannot find appropriate Controller in running OS.");
+				CocoaInput.applyController(new DummyController());
+			}
+			ModLogger.log("CocoaInput has been initialized.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static double getScreenScaledFactor() {
+		return Minecraft.getInstance().getWindow().getGuiScale();
+	}
+
+	public static void applyController(CocoaInputController controller) throws IOException {
+		CocoaInput.controller = controller;
+		ModLogger.log("CocoaInput is now using controller:" + controller.getClass().toString());
+	}
+
+	public static CocoaInputController getController() {
+		return CocoaInput.controller;
+	}
+
+	public void distributeScreen(Object sc) {
+		if (CocoaInput.getController() != null) {
+			CocoaInput.getController().screenOpenNotify((Screen) sc);
+		}
+	}
+
+	public static void copyLibrary(String libraryName, String libraryPath) throws IOException {
+		InputStream libFile;
+		if (zipsource == null) {//Fabric case
+			libFile = CocoaInput.class.getResourceAsStream("/" + libraryPath);
+		} else {
+			try {//Modファイルを検出し、jar内からライブラリを取り出す
+				ZipFile jarfile = new ZipFile(CocoaInput.zipsource);
+				libFile = jarfile.getInputStream(new ZipEntry(libraryPath));
+			} catch (FileNotFoundException e) {//存在しない場合はデバッグモードであるのでクラスパスからライブラリを取り出す
+				ModLogger.log("Couldn't get library path. Is this debug mode?'");
+				libFile = ClassLoader.getSystemResourceAsStream(libraryPath);
+			}
+		}
+		Minecraft mc = Minecraft.getInstance();
+		String nativeDirString = mc.gameDirectory.getAbsolutePath().concat("/native");
+		File nativeDir = new File(nativeDirString);
+		File copyLibFile = new File(nativeDirString.concat("/" + libraryName));
+		try {
+			nativeDir.mkdir();
+			FileOutputStream fos = new FileOutputStream(copyLibFile);
+			copyLibFile.createNewFile();
+			IOUtils.copy(libFile, fos);
+			fos.close();
+		} catch (IOException e1) {
+			ModLogger.error("Attempted to copy library to ./native/" + libraryName + " but failed.");
+		}
+		System.setProperty("jna.library.path", nativeDir.getAbsolutePath());
+		ModLogger.log("CocoaInput has copied library to native directory.");
+	}
+}
---
+++ common/src/main/java/jp/axer/cocoainput/plugin/IMEReceiver.java
@@ -0,0 +1,143 @@
+package jp.axer.cocoainput.plugin;
+
+import java.util.function.BooleanSupplier;
+
+import org.jetbrains.annotations.Nullable;
+import jp.axer.cocoainput.CocoaInput;
+//import jp.axer.cocoainput.util.ModLogger;
+import jp.axer.cocoainput.util.PreeditFormatter;
+import jp.axer.cocoainput.util.Rect;
+import jp.axer.cocoainput.util.Tuple3;
+import net.minecraft.client.Minecraft;
+
+public abstract class IMEReceiver {
+
+	private int length = 0;
+	protected boolean cursorVisible = true;
+	private boolean preeditBegin = false;
+	protected int originalCursorPosition = 0;
+	protected @Nullable BooleanSupplier allowTextDecoration;
+
+	private void replaceMarkedText(String text, int pos, int len)
+	{
+		//ModLogger.debug("replaceMarkedText() ... (new StringBuffer(\"" + this.getText() + "\").replace(" + pos + ", " + (pos + len) + ", \"" + text + "\")");
+		this.setText((new StringBuffer(this.getText()))
+			.replace(pos, pos + len, text).toString());
+	}
+
+	/*
+	 * position1 length1は下線と強調変換のため必須 position2 length2は意味をなしてない
+	 * positionの位置から文字数lengthの範囲という意味
+	 */
+	public void insertText(String aString, int position1, int length1) {//確定文字列 現状aString以外の引数は意味をなしてない
+		//ModLogger.debug("just comming:(\"" + aString + "\") now:(\"" + getText() + "\") length:" + length);
+		if (!preeditBegin) {
+			originalCursorPosition = this.getCursorPos();
+		}
+		preeditBegin = false;
+		cursorVisible = true;
+		replaceMarkedText("", originalCursorPosition, length);
+		length = 0;
+		this.setCursorPos(originalCursorPosition);
+		this.setSelectionPos(originalCursorPosition);
+		if (CocoaInput.config.isNativeCharTyped()) {
+			this.insertTextNative(aString);
+		} else {
+			this.insertTextEmurated(aString);
+		}
+		/*
+		if (aString.length() == 0) {
+			this.setText((new StringBuffer(this.getText()))
+					.replace(originalCursorPosition, originalCursorPosition + length, "").toString());
+			length = 0;
+			this.setCursorPos(originalCursorPosition);
+			this.setSelectionPos(originalCursorPosition);
+			return;
+		}
+		this.setText((new StringBuffer(this.getText()))
+				.replace(originalCursorPosition, originalCursorPosition + length,
+						aString.substring(0, aString.length()))
+				.toString());
+		length = 0;
+		this.setCursorPos(originalCursorPosition + aString.length());
+		this.notifyParent(this.getText());
+		//owner.selectionEnd = owner.cursorPosition;
+		 */
+	}
+
+	public void setMarkedText(String aString, int position1, int length1, int position2, int length2) {
+		if (!preeditBegin) {
+			originalCursorPosition = this.getCursorPos();
+			preeditBegin = true;
+		}
+		int caretPosition;
+		boolean hasCaret;
+		String commitString;
+		if (allowTextDecoration != null && !allowTextDecoration.getAsBoolean()) {
+			hasCaret = false;
+			caretPosition = 0;
+			commitString = aString;
+		}
+		else if (CocoaInput.config.isAdvancedPreeditDraw()) {
+			//ModLogger.debug("PreeditFormatter.formatMarkedText(\"" + aString + "\", " + position1 + ", " + length1 + ")");
+			int max = aString.length();
+			Tuple3<String, Integer, Boolean> formattedText = PreeditFormatter.formatMarkedText(aString,
+				position1 > max ? max : position1,
+				position1 + length1 > max ? max - position1 : length1);
+			commitString = formattedText._1();
+			caretPosition = formattedText._2() + 4;//相対値
+			hasCaret = formattedText._3();
+		}
+		else {
+			hasCaret=true;
+			caretPosition=0;
+			commitString=PreeditFormatter.SECTION+"n"+aString+PreeditFormatter.SECTION+"r";
+		}
+		replaceMarkedText(commitString, originalCursorPosition, length);
+		length = commitString.length();
+		if (hasCaret) {
+			this.cursorVisible = true;
+			this.setCursorPos(originalCursorPosition + caretPosition);
+			this.setSelectionPos(originalCursorPosition + caretPosition);
+		} else {
+			this.cursorVisible = false;
+			this.setCursorInvisible();
+			this.setCursorPos(originalCursorPosition);
+			this.setSelectionPos(originalCursorPosition);
+			//owner.selectionEnd=owner.cursorPosition;
+		}
+	}
+
+	public abstract Rect getRect();
+
+	abstract protected void setText(String text);
+
+	abstract protected String getText();
+
+	abstract protected void setCursorInvisible();
+
+	abstract protected int getCursorPos();
+
+	abstract protected void setCursorPos(int p);
+
+	abstract protected void setSelectionPos(int p);
+
+	protected void insertTextNative(String text) {
+		for (char c : text.toCharArray()) {
+			Minecraft instance = Minecraft.getInstance();
+			instance.keyboardHandler.charTyped(instance.getWindow().getWindow(), c, 0);
+		}
+	}
+
+	protected void insertTextEmurated(String aString) {
+		if (this.getCursorPos() <= this.getText().length()) {
+			replaceMarkedText(aString.substring(0, aString.length()), this.getCursorPos(), 0);
+		}
+		length = 0;
+		this.setCursorPos(this.getCursorPos() + aString.length());
+		this.notifyParent(this.getText());
+	}
+
+	protected void notifyParent(String text) {
+	};
+}
